97d123f80b79e0665fc60d071fcbbef23ec8b467
molodkin8888 <zhenya.molodkin@gmail.com>
2018-01-23 23:14:35 +0300

    create new project-site

--- index1_1.js
+++ index1_1.js
@@ -1,19 +1,11 @@
-var time = 2;
-$('#counter').each(function(){
-  $('div').each(function(){
-    var 
-    i = 1,
-    num = $(this).data('num'),
-    step = 1000 * time / num,
-    that = $(this),
-    int = setInterval(function(){
-      if (i <= num) {
-        that.html(i);
-      }
-      else {
-        clearInterval(int);
-      }
-      i++;
-    },step);
+$('.number').each(function(){
+  $(this).prop('Counter', 0).animate({
+    Counter:$(this).text()
+  }, {
+    duration: 4000,
+    easing: 'swing',
+    step:function(now){
+      $(this).text(Math.ceil(now));
+    }
   });
 });
